99baf04aa0acd6f4c4612fd0461e37d5234dff0e
umair <umair81307@gmail.com>
2020-07-27 13:58:59 +0530

    project 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Keypad from './components/keypad';
+import Output from './components/output';
+import Header from './components/header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    result: " "
+  };
+  buttonPressed = buttonName => {
+    if (buttonName === "=") {
+      this.calculate();
+    } else 
+    if (buttonName === "C") {
+      this.reset()
+    } else if (buttonName === "CE") {
+      this.backspace();
+    } else
+      this.setState({
+        result: this.state.result + buttonName
+      });
+  };
+backspace = () => {
+  this.setState({
+    result: this.state.result.slice(0, -1)
+  })
+}
+  reset = () => {
+    this.setState({
+      result: ""
+    });
+  }
+  calculate = () => {
+    try {
+      this.setState({
+        result: (eval(this.state.result) || "") + ""
+      });
+    } catch (e) {
+      this.setState({
+        result: "error"
+      });
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <div className="calc-body">
+          <Output result={this.state.result} />
+          <Keypad buttonPressed={this.buttonPressed} />
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component{
+    render(){
+        return(
+            <div className="header">
+                <h1>Simple Calculator</h1>
+            </div>
+        )
+    }
+}
+
+export default Header;
